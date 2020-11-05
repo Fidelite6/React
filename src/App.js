f95f1674a77c9cba9607dba4973b5f7c6194a5e8
fidelite <yhalias@artelogic.net>
2020-11-05 20:22:53 +0200

    clean-up everything

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className={styles.center}>
-      <header className={styles.header}>
-        <img src={logo} className={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles.link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      learn react
     </div>
   );
 }
