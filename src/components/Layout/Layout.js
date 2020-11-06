5faf93c39f4fb6579c949b860fb11310efda0921
fidelite <yhalias@artelogic.net>
2020-11-06 20:47:26 +0200

    added everything needed for toolbar and style.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Layout.module.css';
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = (props) => (
-  <>
-    <Toolbar/>
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </>
-);
+class layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false})
+  }
+
+  sideDrawerOpenedHandler = () => {
+    this.setState((prevState) => {
+      return {showSideDrawer: !prevState.showSideDrawer}
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <Toolbar showSideDrawer={this.sideDrawerOpenedHandler}/>
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}/>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </>
+    )
+  }
+}
 
 export default layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
 const toolbar = (props) => {
   return (
     <header className={classes.Toolbar}>
-      <div>MENU</div>
-      <Logo/>
-      <nav>
-        ...
+      <DrawerToggle click={props.showSideDrawer}/>
+      <div className={[classes.Logo, classes.DesktopOnly].join(' ')}>
+        <Logo/>
+      </div>
+      <nav className={classes.DesktopOnly}>
+        <NavigationItems/>
       </nav>
     </header>
   );
