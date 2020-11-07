004b7f0d3880111097a328780c7f46000204a264
fidelite <yhalias@artelogic.net>
2020-11-07 15:09:01 +0200

    shouldComponentUpdate improvement for Modal react <16.8

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.module.css';
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = (props) => {
-  return (
-    <>
-      <div
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0',
-        }}
-        className={classes.Modal}
-      >
-        {props.children}
-      </div>
-      <Backdrop
-        show={props.show}
-        clicked={props.modalClosed}
-      />
-    </>
-  );
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    return (
+      <>
+        <div
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0',
+          }}
+          className={classes.Modal}
+        >
+          {this.props.children}
+        </div>
+        <Backdrop
+          show={this.props.show}
+          clicked={this.props.modalClosed}
+        />
+      </>
+    );
+  }
 }
 
-export default modal;
+export default Modal;
