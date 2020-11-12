9ee91d9d91479ed74f28c82d4614b5816c68986b
fidelite <yhalias@artelogic.net>
2020-11-12 23:34:08 +0200

    added checkout page and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Layout from "./containers/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import { Route, Switch } from 'react-router-dom'
+import Checkout from "./containers/Checkout/Checkout";
 
 function App() {
   return (
     <Layout>
-      <BurgerBuilder/>
+      <Switch>
+        <Route path='/checkout' component={Checkout}/>
+        <Route path='/' exact component={BurgerBuilder}/>
+      </Switch>
     </Layout>
   );
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -83,26 +83,17 @@ class BurgerBuilder extends Component {
   purchaseContinue = () => {
     this.setState({loading: true});
 
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'test',
-        address: {
-          street: 'test street',
-          country: 'Ukraine'
-        },
-      },
-      email: 'test@test.com'
-    };
-
-    axios.post('/orders', order)
-      .then(response => {
-        this.setState({loading: false, purchasing: false});
-      })
-      .catch(error => {
-        this.setState({loading: false, purchasing: false});
-      });
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      const string = encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]);
+      queryParams.push(string);
+    }
+    queryParams.push('price=' + this.state.totalPrice);
+    const queryString = queryParams.join('&');
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString,
+    });
   }
 
   render() {
