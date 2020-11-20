c5a30487cb4c249395592b3bdce1047b46ddd0b4
fidelite <yhalias@artelogic.net>
2020-11-20 15:50:16 +0200

    minor refactoring reducer

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from "../utility";
 
 const initialState = {
   orders: [],
@@ -9,47 +10,30 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.FETCH_ORDER_START:
-      return {
-        ...state,
-        loading: true
-      };
+      return updateObject(state, {loading: true});
     case actionTypes.FETCH_ORDER_SUCCESS:
-      return {
-        ...state,
+      return updateObject(state, {
         orders: action.payload.orders,
-        loading: false,
-      };
+        loading: false
+      });
     case actionTypes.FETCH_ORDER_FAILED:
-      return {
-        ...state,
-        loading: false,
-      };
+      return updateObject(state, {loading: false});
     case actionTypes.PURCHASE_INIT:
-      return {
-        ...state,
-        purchased: false,
-      };
+      return updateObject(state, {purchased: false});
     case actionTypes.PURCHASE_BURGER_START:
-      return {
-        ...state,
-        loading: true,
-      };
+      return updateObject(state, {purchased: true});
     case actionTypes.PURCHASE_BURGER_SUCCESS:
       const newOrder = {
         ...action.payload.orderData,
         id: action.payload.id,
       };
-      return {
-        ...state,
+      return updateObject(state, {
         loading: false,
         orders: state.orders.concat(newOrder),
         purchased: true,
-      };
+      });
     case actionTypes.PURCHASE_BURGER_FAIL:
-      return {
-        ...state,
-        loading: false,
-      };
+      return updateObject(state, {loading: false});
     default:
       return state;
   }
